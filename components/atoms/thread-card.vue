ea4ada71620e484e96c686b12b6b87d56dc4c5cb
kou0402 <kou.4869.biz@gmail.com>
2020-03-01 23:46:05 +0900

    feat: update index component

---
+++ components/atoms/thread-card.vue
@@ -0,0 +1,13 @@
+<template>
+  <nuxt-link to="/">
+    <div class="card">
+      <div class="card-content">
+        <div class="level is-mobile">
+          <div class="level-item">Vue</div>
+          <div class="level-item">VS</div>
+          <div class="level-item">React</div>
+        </div>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
---
+++ components/organisms/hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          ツール比較コミュニケーションサイト
+        </h1>
+        <h2 class="subtitle">
+          二つのテーマを持ち寄って意見をぶつけ合おう！
+        </h2>
+        <button class="button is-primary">スレッドを作成する</button>
+      </div>
+    </div>
+  </section>
+</template>
