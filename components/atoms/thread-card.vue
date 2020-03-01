44f65cdc16e6c847c7c98614c4779251af297802
kou0402 <kou.4869.biz@gmail.com>
2020-03-01 23:54:23 +0900

    feat: update index component

--- components/atoms/thread-card.vue
+++ components/atoms/thread-card.vue
@@ -1,13 +1,28 @@
 <template>
-  <nuxt-link to="/">
+  <nuxt-link to="/" class="thread-card">
     <div class="card">
       <div class="card-content">
         <div class="level is-mobile">
           <div class="level-item">Vue</div>
-          <div class="level-item">VS</div>
+          <img class="level-item vs" src="~/assets/images/vs.svg" />
           <div class="level-item">React</div>
         </div>
       </div>
     </div>
   </nuxt-link>
 </template>
+
+<style lang="scss" scoped>
+.thread-card {
+  .card {
+    .card-content {
+      .level {
+        .vs {
+          width: 30px;
+          height: 30px;
+        }
+      }
+    }
+  }
+}
+</style>
