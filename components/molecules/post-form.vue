10c190651e1fe8fc20f7da0ceaba164328e0d713
kou0402 <kou.4869.biz@gmail.com>
2020-03-12 02:43:49 +0900

    feat: add thread page

---
+++ components/molecules/post-form.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="post-form">
+    <div class="form-area">
+      <div class="columns is-mobile select-area">
+        <div class="column is-5 is-offset-1">
+          <button class="button is-small is-fullwidth">Vue.js</button>
+        </div>
+        <div class="column is-5 input-area">
+          <button class="button is-small is-fullwidth">Vue.js</button>
+        </div>
+      </div>
+      <div class="columns is-mobile">
+        <div class="column is-8 is-offset-1">
+          <input class="input is-small" type="text" />
+        </div>
+        <div class="column is-2">
+          <img src="~/assets/images/right-arrow.svg" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.post-form {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 12%;
+  background-color: $main-color4;
+  > .form-area {
+    > .select-area {
+      margin-top: 0;
+      margin-bottom: 0;
+      > .column {
+        padding-top: 10px;
+        padding-bottom: 10px;
+      }
+    }
+    > .input-area {
+      margin-top: 0;
+      > .column {
+        padding-top: 0;
+      }
+    }
+  }
+}
+</style>
---
+++ pages/thread.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="thread">
+    <ThreadCard></ThreadCard>
+    <PostCard side="left"></PostCard>
+    <PostCard side="right"></PostCard>
+    <PostCard side="left"></PostCard>
+    <PostCard side="right"></PostCard>
+    <PostCard side="left"></PostCard>
+    <PostCard side="right"></PostCard>
+    <PostForm></PostForm>
+  </main>
+</template>
+
+<script>
+import ThreadCard from '~/components/molecules/thread-card.vue'
+import PostCard from '~/components/molecules/post-card.vue'
+import PostForm from '~/components/molecules/post-form.vue'
+
+export default {
+  components: {
+    ThreadCard,
+    PostCard,
+    PostForm
+  }
+}
+</script>
