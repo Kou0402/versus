d2b2ede9392ccc8a2b8f98d1f005707b2ee62524
kou0402 <kou.4869.biz@gmail.com>
2020-03-12 23:52:36 +0900

    feat: add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="contact">
+    <h2>お問い合わせ</h2>
+    <p>本サービスに対する</p>
+    <p>ご意見・お問い合わせは</p>
+    <p>こちらのアカウントより承っております。</p>
+    <img class="contact-icon" src="~/assets/images/twitter.svg" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.contact {
+  > .contact-icon {
+    width: 50px;
+  }
+}
+</style>
