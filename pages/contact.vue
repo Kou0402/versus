2e9e1dd277fa7819e595caf084ecd44f16bdfd93
kou0402 <kou.4869.biz@gmail.com>
2020-03-22 19:33:26 +0900

    feat: add contact page logic

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,7 +4,13 @@
     <p>本サービスに対する</p>
     <p>ご意見・お問い合わせは</p>
     <p>こちらのアカウントより承っております。</p>
-    <img class="contact-icon" src="~/assets/images/twitter.svg" />
+    <a
+      href="https://twitter.com/ko_u_0402"
+      class="twitter-link"
+      target="_blank"
+    >
+      <img class="contact-icon" src="~/assets/images/twitter.svg" />
+    </a>
   </main>
 </template>
 
@@ -12,6 +18,9 @@
 .contact {
   > .contact-icon {
     width: 50px;
+  }
+  > .twitter-link img {
+    width: 70px;
   }
 }
 </style>
